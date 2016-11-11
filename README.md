727b1cc7d71bd3689777a863d75b12de6eb655b9
liuhuan <liuwei41255703@163.com>
2016-11-11 18:17:33 +0800

    添加 maven 插件。

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 1.进入Equinox的控制台
   
     java –jar org.eclipse.osgi_3.7.0.v20110613.jar -console
+
+
+2.Bundle-Name是给用户读的，而Bundle-SymbolicName是给OSGi框架读的，让OSGi框架能够唯一标识一个bundle。
+
+3.打包 ：
+    
+    mvn clean install pax:provision
---
+++ src/main/java/com/ryan/osgi/activator/impl/HelloActivatorImpl.java
@@ -0,0 +1,20 @@
+package com.ryan.osgi.activator.impl;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Rayn
+ * @email liuwei412552703@163.com
+ * Created by Rayn on 2016/11/11 18:08.
+ */
+public class HelloActivatorImpl implements BundleActivator {
+
+    public void start(BundleContext bundleContext) throws Exception {
+
+    }
+
+    public void stop(BundleContext bundleContext) throws Exception {
+
+    }
+}
