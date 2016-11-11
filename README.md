b7e33dfbfafbee6a5e214917d4e7fa5c6d2f8086
liuhuan <liuwei41255703@163.com>
2016-11-11 17:52:39 +0800

    项目初始化。

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Eclipse OSGi Framework
 ============================================================================
 
-
+# 控制台
+1.进入Equinox的控制台
+  
+    java –jar org.eclipse.osgi_3.7.0.v20110613.jar -console
---
+++ src/main/java/com/ryan/osgi/hello/Hello.java
@@ -0,0 +1,16 @@
+package com.ryan.osgi.hello;
+
+/**
+ * @author Rayn
+ * @email liuwei412552703@163.com
+ * Created by Rayn on 2016/11/11 14:34.
+ */
+public interface Hello {
+
+    /**
+     *
+     * @param name
+     * @return
+     */
+    public String sayHello(String name);
+}
